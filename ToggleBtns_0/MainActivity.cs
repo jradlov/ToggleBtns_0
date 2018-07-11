a000d4e1074174b2dde353ac4b7d352bfd5970bc
Jaime Radulovich <jradlov@gmail.com>
2018-07-11 08:01:45 -0700

    Add project files.

---
+++ ToggleBtns_0/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+
+namespace ToggleBtns_0
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var toggleBtn = FindViewById<ToggleButton>(Resource.Id.toggleButton1);
+
+            toggleBtn.CheckedChange += delegate {
+                Log.Debug("----DEBUG----", "toggle btn checked? " + toggleBtn.Checked.ToString());
+            };
+        }
+    }
+}
+
